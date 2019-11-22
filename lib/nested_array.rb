75d07f593dc8fa2039f74646a545d7331aa42c42
Radouane Bahi <radbahi@gmail.com>
2019-11-22 22:28:02 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -31,7 +31,7 @@ ENFIELD_TENNIS_ACADEMY.sort
 end
 
 def matrix_lookup(matrix, row, column)
-  # Return the matrix's content at row and column
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
